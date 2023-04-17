530d74e15459237682e57c1f69aec09c14bfe736
oinarisummer <oinarisummer888@gmail.com>
2023-04-18 01:13:41 +0900

    initial commint

---
+++ QuizCounter/Data/CounterModel.cs
@@ -0,0 +1,19 @@
+namespace QuizCounter.Data;
+
+public class CounterModel
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public int Correct { get; set; } = 0;
+
+    public int Worng { get; set; } = 0;
+
+    public int Score { get; set; } = 0;
+
+    public void Reset()
+    {
+        Score = 0;
+        Correct = 0;
+        Worng = 0;
+    }
+}
