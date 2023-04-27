729d2532dd4745b2a0992c63c4af0e53d0e4593f
oinarisummer <oinarisummer888@gmail.com>
2023-04-27 22:34:47 +0900

    add UndoButton

--- QuizCounter/Model/CounterModel.cs
+++ QuizCounter/Model/CounterModel.cs
@@ -10,10 +10,28 @@ public class CounterModel
 
     public int Score { get; set; } = 0;
 
+    public Stack<(int correct, int worng, int score)> UndoStack = new Stack<(int, int, int)>();
+
+    public virtual void PushUndoStack()
+    {
+        UndoStack.Push((Correct, Worng, Score));
+    }
+
+    public void Undo()
+    {
+        if (UndoStack.TryPop(out (int correct, int worng, int score) previousPoint))
+        {
+            Correct = previousPoint.correct;
+            Worng = previousPoint.worng;
+            Score = previousPoint.score;
+        }
+    }
+
     public void Reset()
     {
         Score = 0;
         Correct = 0;
         Worng = 0;
+        UndoStack.Clear();
     }
 }
